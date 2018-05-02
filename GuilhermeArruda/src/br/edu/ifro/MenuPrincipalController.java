7e78854a56d473b14931c9c2ed484d3b50b31cd5
Guilherme Arruda <>
2018-05-02 11:13:21 -0400

    Instalando biblioteca do Hibernate no projeto.

--- GuilhermeArruda/src/br/edu/ifro/MenuPrincipalController.java
+++ GuilhermeArruda/src/br/edu/ifro/MenuPrincipalController.java
@@ -1,16 +1,19 @@
-/*
+    /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package br.edu.ifro;
 
+
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 /**
  *
@@ -19,12 +22,18 @@ import javafx.scene.control.Label;
 public class MenuPrincipalController implements Initializable {
     
     @FXML
-    private Label label;
+    private TextField txtNúmero1, txtNúmero2, txtResultado;
+    
+     @FXML
+    private Button btnSoma;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private void soma(ActionEvent event) {
+        Double num1= Double.parseDouble(txtNúmero1.getText());
+        Double num2= Double.parseDouble(txtNúmero2.getText());
+        Double resultado = num1+num2;
+        
+        txtResultado.setText(resultado.toString());
     }
     
     @Override
